8255efdaec25e06b330f7c2783b6c149329d3a6a
rosemelo <rosenildesmelo@gmail.com>
2023-12-11 18:54:36 -0300

    Mudança de planos

---
+++ projeto_react_campinho/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+function Home() {
+    return (
+        <div>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/08859eaa-cda3-43d7-9ceb-ef5ee90e3b17/dg8t7hh-45fca3f2-5c57-4c01-97b8-54ea3ddf8a3e.jpg/v1/fit/w_375,h_516,q_70,strp/peter_pan_by_sapienstoonz_dg8t7hh-375w.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvMDg4NTllYWEtY2RhMy00M2Q3LTljZWItZWY1ZWU5MGUzYjE3XC9kZzh0N2hoLTQ1ZmNhM2YyLTVjNTctNGMwMS05N2I4LTU0ZWEzZGRmOGEzZS5qcGciLCJ3aWR0aCI6Ijw9NTgxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.AK6LUeImMPzBDO-nyGEsm4w3lr7NlgUrBQ1JwPiyZ5I" alt="PeterPan" />
+                    <span></span>
+                </li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default Home;
